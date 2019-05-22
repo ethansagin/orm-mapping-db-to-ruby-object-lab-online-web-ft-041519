845f82966832c832bc9169de6e672c4a33b33063
Ethan Sagin <ethansagin@gmail.com>
2019-05-22 20:26:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,4 +85,14 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM Students
+      WHERE grade = 10 
+      LIMIT ?
+    SQL
+    
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
 end
