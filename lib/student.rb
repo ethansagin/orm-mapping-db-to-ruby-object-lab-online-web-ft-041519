865f21d8040b61fde6849e6a463ecb752067852e
Ethan Sagin <ethansagin@gmail.com>
2019-05-22 20:24:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,6 +59,17 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM Students
+      WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 end
-
-
